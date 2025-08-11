0d9d9cb394ab370d9e45d0b052fc6ac8751c091b
spinpah <aymene16boudjelida@gmail.com>
2025-08-11 17:30:46 +0100

    ..

--- app/(with-layout)/visitors/page.tsx
+++ app/(with-layout)/visitors/page.tsx
@@ -2,10 +2,15 @@
 import { useState, useEffect, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error("Missing Supabase environment variables");
+}
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+
 
 export default function StickerBoard() {
   const [name, setName] = useState("");
